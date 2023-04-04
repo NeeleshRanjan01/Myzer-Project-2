57b15b07323881b7df78980dad5961d52c9a1155
Neelesh Ranjan <neeleshranjan95@gmail.com>
2023-04-04 21:18:47 +0530

    first commit

--- index.js
+++ index.js
@@ -14,39 +14,33 @@ const mongoose = require("mongoose")
 const { ResponseModel } = require("./MongSchema")
 const { url } = require("./MongSchema");
 
-//post the updated data in database
-
-app.post("/myzer", (async (req, res) => {
-    try {
-        await mongoose.connect(url)
-        let updateData = req.body;
-        await ResponseModel.updateMany(
-            { "_id": "642c00e71a174dbaf285d000" },
-            { $set: updateData })
-        res.send(updateData)
-    }
-    catch (error) {
-        console.log(error);
-    }
-}))
-
-//get data on the UI 
 
 app.get("/", (async (req, res) => {
-    try {
-        await mongoose.connect(url)
-        const stringData = await ResponseModel.find({ "_id": "642c00e71a174dbaf285d000" })
 
-        var data = `<h1>welcome_txt : ${stringData[0].welcome_txt}</h1><br> 
+    await mongoose.connect(url)
+    const stringData = await ResponseModel.find({ "_id": "642c00e71a174dbaf285d000" })
+
+    var data = `<h1>welcome_txt : ${stringData[0].welcome_txt}</h1><br> 
         <h1>name : ${stringData[0].name}</h1><br>
         <h1>paragraph : ${stringData[0].paragraph}</h1><br>
         <h1>country : ${stringData[0].country}</h1><br>
         <h1>description : ${stringData[0].description}</h1><br>`
-        res.send(data)   
-    }
-    catch (error) {
-        console.log(error);
-    } 
+    res.send(data)
+
 }))
+
+//post the updated data in database
+app.post("/myzer", (async (req, res) => {
+
+    await mongoose.connect(url)
+    let updateData = req.body;
+    await ResponseModel.updateMany(
+        { "_id": "642c00e71a174dbaf285d000" },
+        { $set: updateData })
+    res.send(updateData)
+
+}))
+
+//get data on the UI 
 
 app.listen(process.env.PORT || 4000)
