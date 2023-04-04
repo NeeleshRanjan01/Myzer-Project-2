0f9426fd202acba60a88803433a9bab00da1dcb7
Neelesh Ranjan <neeleshranjan95@gmail.com>
2023-04-04 22:06:36 +0530

    first commit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const { ResponseModel } = require("./MongSchema")
 const { url } = require("./MongSchema");
 
 // connect to the database
-mongoose.connect(url)
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to database')
   })
@@ -27,10 +27,14 @@ mongoose.connect(url)
 app.post("/myzer", async (req, res) => {
   try {
     let updateData = req.body;
-    await ResponseModel.updateMany(
+    const result = await ResponseModel.updateMany(
       { "_id": "642c00e71a174dbaf285d000" },
       { $set: updateData })
-    res.send(updateData)
+    if (result.nModified > 0) {
+      res.send(updateData)
+    } else {
+      res.status(404).send('No document found')
+    }
   } catch (error) {
     console.log(error);
     res.status(500).send("Internal server error")
@@ -41,13 +45,16 @@ app.post("/myzer", async (req, res) => {
 app.get("/", async (req, res) => {
   try {
     const stringData = await ResponseModel.find({ "_id": "642c00e71a174dbaf285d000" })
-
-    var data = `<h1>welcome_txt : ${stringData[0].welcome_txt}</h1><br> 
-        <h1>name : ${stringData[0].name}</h1><br>
-        <h1>paragraph : ${stringData[0].paragraph}</h1><br>
-        <h1>country : ${stringData[0].country}</h1><br>
-        <h1>description : ${stringData[0].description}</h1><br>`
-    res.send(data)
+    if (stringData.length > 0) {
+      var data = `<h1>welcome_txt : ${stringData[0].welcome_txt}</h1><br> 
+          <h1>name : ${stringData[0].name}</h1><br>
+          <h1>paragraph : ${stringData[0].paragraph}</h1><br>
+          <h1>country : ${stringData[0].country}</h1><br>
+          <h1>description : ${stringData[0].description}</h1><br>`
+      res.send(data)
+    } else {
+      res.status(404).send('No document found')
+    }
   } catch (error) {
     console.log(error);
     res.status(500).send("Internal server error")
