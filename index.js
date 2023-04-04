89c6e2a2ad823a7ec3a1710c825244f7aa1ec38b
Neelesh Ranjan <neeleshranjan95@gmail.com>
2023-04-04 21:49:11 +0530

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const bodyParser = require('body-parser');
 const express = require("express");
 const cors = require("cors")
-const mongoose = require("mongoose")
 const app = express();
 app.use(cors());
 app.use(express.json())
@@ -11,24 +10,25 @@ app.use(bodyParser.urlencoded({
 }));
 
 // configuring mongoose
+const mongoose = require("mongoose")
 const { ResponseModel } = require("./MongSchema")
 const { url } = require("./MongSchema");
 
 //post the updated data in database
 
-// app.post("/myzer", (async (req, res) => {
-//     try {
-//         await mongoose.connect(url)
-//         let updateData = req.body;
-//         await ResponseModel.updateMany(
-//             { "_id": "642c00e71a174dbaf285d000" },
-//             { $set: updateData })
-//         res.send(updateData)
-//     }
-//     catch (error) {
-//         console.log(error);
-//     }
-// }))
+app.post("/myzer", (async (req, res) => {
+    try {
+        await mongoose.connect(url)
+        let updateData = req.body;
+        await ResponseModel.updateMany(
+            { "_id": "642c00e71a174dbaf285d000" },
+            { $set: updateData })
+        res.send(updateData)
+    }
+    catch (error) {
+        console.log(error);
+    }
+}))
 
 //get data on the UI 
 
@@ -36,20 +36,17 @@ app.get("/", (async (req, res) => {
     try {
         await mongoose.connect(url)
         const stringData = await ResponseModel.find({ "_id": "642c00e71a174dbaf285d000" })
-        res.send("hooo")
 
-        // var data = `<h1>welcome_txt : ${stringData[0].welcome_txt}</h1><br> 
-        // <h1>name : ${stringData[0].name}</h1><br>
-        // <h1>paragraph : ${stringData[0].paragraph}</h1><br>
-        // <h1>country : ${stringData[0].country}</h1><br>
-        // <h1>description : ${stringData[0].description}</h1><br>`
-        // res.send(data)   
+        var data = `<h1>welcome_txt : ${stringData[0].welcome_txt}</h1><br> 
+        <h1>name : ${stringData[0].name}</h1><br>
+        <h1>paragraph : ${stringData[0].paragraph}</h1><br>
+        <h1>country : ${stringData[0].country}</h1><br>
+        <h1>description : ${stringData[0].description}</h1><br>`
+        res.send(data)   
     }
     catch (error) {
         console.log(error);
     } 
-
-    
 }))
 
 app.listen(process.env.PORT || 4000)
